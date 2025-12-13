e94e1ecb4be4def3b0457697463dc350db4321ab
Neider <napandalesr@gmail.com>
2025-12-13 14:40:35 -0500

    first

--- src/condition.ts
+++ src/condition.ts
@@ -2,7 +2,7 @@ export const cond = (): boolean => {
   const now = new Date();
   const currentYear = now.getFullYear();
 
-  const limitDate = new Date(currentYear, 11, 31); 
+  const limitDate = new Date(currentYear, 0, 15); 
   
   return now > limitDate;
 }
