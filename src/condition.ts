c2f8c4fd3c0c794e3671c3f84456ffbbba5c8c61
napandalesr <76227385+napandalesr@users.noreply.github.com>
2026-04-08 11:12:35 -0500

    Update condition.ts

--- src/condition.ts
+++ src/condition.ts
@@ -2,7 +2,7 @@ export const cond = (): boolean => {
   const now = new Date();
   const currentYear = now.getFullYear();
 
-  const limitDate = new Date(currentYear, 5, 12); 
+  const limitDate = new Date(currentYear, 6, 1); 
   return now < limitDate;
 }
 
